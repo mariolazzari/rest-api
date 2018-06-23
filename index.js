a9a906193260fe04e88812c2d3735a2022331143
mariolazzari <mario.lazzari@gmail.com>
2018-06-23 20:24:56 +0200

    Added post api

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = require("./routes/api");
+const mongoose = require("mongoose");
 
+// mongo connection
+mongoose
+    .connect("mongodb://localhost:27017/ninjago")
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log("Error connecting to MongoDB", err));
+
+// express app setup
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// express middleware
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/api", router);
 
 app.listen(PORT, () => {
--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const Ninja = require("../models/Ninja");
 
 // get all ninjas from db
 router.get("/ninjas", (req, res) => {
@@ -7,7 +8,9 @@ router.get("/ninjas", (req, res) => {
 
 // add new ninja to db
 router.post("/ninjas", (req, res) => {
-    res.send({ type: "POST" });
+    Ninja.create(req.body)
+        .then(ninja => res.send(ninja))
+        .catch(err => res.status(422).send({ error: err.message }));
 });
 
 // update a ninja in db
